a7dce58bdb4e23eef65941d57839a4a7449e5716
peter nadel <peter.m.nadel@gmail.com>
2023-01-09 11:28:24 -0500

    manyoshu

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ import spacy
 from spacy.tokens import DocBin
 
 st.title('Utsusemi: Japanese Dependency Parsing')
-st.markdown('Searching [源氏物語 -- The Tale of Genji](https://ja.wikisource.org/wiki/%E6%BA%90%E6%B0%8F%E7%89%A9%E8%AA%9E_(%E6%B8%8B%E8%B0%B7%E6%A0%84%E4%B8%80%E6%A0%A1%E8%A8%82))')
+st.markdown('Searching [万葉集](https://jti.lib.virginia.edu/japanese/manyoshu/AnoMany.html)')
 
 # data
 @st.cache(allow_output_mutation=True)
@@ -15,13 +15,13 @@ nlp = load_model()
 
 @st.cache(allow_output_mutation=True)
 def get_data():
-    mm_from_csv = pd.read_csv('tog_sents.csv')
-    return mm_from_csv
-mm_from_csv = get_data()
+    from_csv = pd.read_csv('manyoshu.csv')
+    return from_csv
+from_csv = get_data()
 
-bytes_file = open('serialized_data/spacy_model_output','rb').read()
+bytes_file = open('serialized_data/manyoshu_spacy_output','rb').read()
 doc_bin = DocBin().from_bytes(bytes_file)
-mm_from_csv['spacy_docs'] =  pd.Series(doc_bin.get_docs(nlp.vocab))
+from_csv['spacy_docs'] =  pd.Series(doc_bin.get_docs(nlp.vocab))
 
 # search
 def word_search(search, spacy_doc, lemma_search=False):
@@ -48,7 +48,7 @@ def word_search(search, spacy_doc, lemma_search=False):
 lemma_search = st.checkbox('Search lemmas')
 search = st.text_input('Search term', '空蝉')
 
-for sd in mm_from_csv['spacy_docs'].to_list():
+for i,sd in enumerate(from_csv['spacy_docs'].to_list()):
     word_search(search.lower(), sd, lemma_search)
 
-st.markdown('<small style="text-align: center">Assembled by Peter Nadel | TTS Research Technology</small>', unsafe_allow_html=True)
+st.markdown('<small>Assembled by Peter Nadel | TTS Research Technology</small>', unsafe_allow_html=True)
